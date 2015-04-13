f7e002b01b88dc836d046ee5fe115f7a0d992fcc
Frank Xu <frankxu@Franks-MacBook-Pro-2.local>
2015-04-13 14:50:05 -0400

    add data base

--- crm.rb
+++ crm.rb
@@ -1,9 +1,5 @@
-require_relative 'rolodex'
 require 'sinatra'
 require 'data_mapper'
-
-$rolodex = Rolodex.new
-
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
@@ -26,6 +22,7 @@ get '/' do
 end
 
 get '/contacts' do
+	@contacts = Contact.all
 	@crm_app_name = "Frank's CRM"
 	erb :contacts
 end
@@ -37,39 +34,41 @@ end
 
 get '/contacts/:id/edit' do
 	@crm_app_name = "Frank's CRM"
-	@contact = $rolodex.find(params[:id])
+	@contact = Contact.get(params[:id])
 	erb :modify_contact
 end
 
 get '/contacts/:id' do
 	@crm_app_name = "Frank's CRM"
-	@contact = $rolodex.find(params[:id])	
+	@contact = Contact.get(params[:id])	
 	erb :show
 end
 
 put '/contacts/:id' do
-	contact = $rolodex.find(params[:id])
+	contact = Contact.get(params[:id])
 	contact.first_name = params[:first_name]
 	contact.last_name = params[:last_name]
 	contact.email = params[:email]
 	contact.notes = params[:notes]
-
+	contact.save
 	redirect to ('/contacts')
 end
 
 post '/contacts' do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-    $rolodex.add_contact(new_contact)
+	contact = Contact.create({
+		:first_name => params[:first_name], 
+		:last_name => params[:last_name], 
+		:email =>params[:email], 
+		:notes => params[:notes]
+		})
+   
     redirect to('/contacts')
 end
 
 
 delete '/contacts/:id' do
-	contact = $rolodex.find(params[:id])
-	$rolodex.delete_contact(contact)
+	contact = Contact.get(params[:id])
+	contact.destroy
 	redirect to('/contacts')
 end
-
-$rolodex.add_contact(Contact.new("Matt", "Holtom", "matt@gmail.com", "sad"))
-$rolodex.add_contact(Contact.new("Frank", "Xu", "frank@gmail.com", "happy"))
 
